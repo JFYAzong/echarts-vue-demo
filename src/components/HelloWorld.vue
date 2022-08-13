6a5eea6fab8ea72e84aff64066e1e39f8284a3e2
JFYAzong <1018720708@qq.com>
2022-08-13 20:57:29 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,98 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="echarts" id="main"></div>
 </template>
 
 <script>
+import * as echarts from "echarts";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  mounted() {
+    var myChart = echarts.init(document.getElementById("main"));
+
+    var option = {
+      title: {
+        text: "ECharts 入门案例",
+      },
+      tooltip: {},
+      legend: {
+        data: ["销量"],
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    };
+    myChart.setOption(option);
+  },
+};
+
+// import ECharts from 'vue-echarts'
+// import 'echarts/lib/chart/line'
+// import 'echarts/lib/component/polar'
+
+// export default {
+//   components: {
+//     'v-chart': ECharts
+//   },
+//   data () {
+//     let data = []
+
+//     for (let i = 0; i <= 360; i++) {
+//         let t = i / 180 * Math.PI
+//         let r = Math.sin(2 * t) * Math.cos(2 * t)
+//         data.push([r, i])
+//     }
+
+//     return {
+//       polar: {
+//         title: {
+//           text: '极坐标双数值轴'
+//         },
+//         legend: {
+//           data: ['line']
+//         },
+//         polar: {
+//           center: ['50%', '54%']
+//         },
+//         tooltip: {
+//           trigger: 'axis',
+//           axisPointer: {
+//             type: 'cross'
+//           }
+//         },
+//         angleAxis: {
+//           type: 'value',
+//           startAngle: 0
+//         },
+//         radiusAxis: {
+//           min: 0
+//         },
+//         series: [
+//           {
+//             coordinateSystem: 'polar',
+//             name: 'line',
+//             type: 'line',
+//             showSymbol: false,
+//             data: data
+//           }
+//         ],
+//         animationDuration: 2000
+//       }
+//     }
+//   }
+// }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.echarts {
+  width: 600px;
+  height: 400px;
 }
 </style>
